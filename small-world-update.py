48c34418439357ea8f5436a6ad8d2af002f3ac67
erkinpolat <44356468+erkinpolat@users.noreply.github.com>
2020-02-24 12:59:44 -0300

    Create small-world-update.py

---
+++ small-world-update.py
@@ -0,0 +1,37 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from pylab import *
+import networkx as nx
+import random as rd
+
+dim=[5,6]
+
+def initialize():
+    global g
+    g = nx.grid_graph(dim) 
+    g.pos = nx.spring_layout(g)
+    g.count = 0
+
+def observe():
+    global g
+    cla()
+    nx.draw(g, pos = g.pos)
+
+def update():
+    global g
+    g.count += 1
+    if g.count % 20 == 0: # rewiring once in every 20 steps
+        nds = list(g.nodes)
+        i = rd.choice(nds)
+        if g.degree[i] > 0:
+            g.remove_edge(i, rd.choice(list(g.neighbors(i))))
+            nds.remove(i)
+            for j in g.neighbors(i):
+                nds.remove(j)
+            g.add_edge(i, rd.choice(nds))
+
+    # simulation of node movement
+    g.pos = nx.spring_layout(g, pos = g.pos, iterations = 5)
+
+import pycxsimulator
+pycxsimulator.GUI().start(func=[initialize, observe, update])
